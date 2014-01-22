71e059b5bcf5d74097f30a65776e3e58ed20bb7f
smarsh25 <sjm.atl@gmail.com>
2014-01-21 21:00:31 -0800

    whoa. waited too long on commits. but knocked out edit,delete,list.

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,15 +1,73 @@
 class CreaturesController < ApplicationController
 
+	# main page. displays all creatures.
 	def index
+		# collect all created creatures from DB, then dispaly on main index page
+		@creatures = Creature.all
 		render :index 
 	end
 
+	# main page. displays all creatures.
+	def list
+		# collect all created creatures from DB, then dispaly on main index page
+		@creatures = Creature.all
+		render :list 
+	end
+
+	# show a form to capture data for a new creature
 	def new
 		render :new
 	end
 
+	# collect info from create form, save it, then redirect to index showing all creatures
 	def create
-		redirect_to '/creatures'
+		# collect the information entered on the form, via params (returns a hash)
+		creature = params.require(:creature).permit(:name, :description)
+		Creature.create(creature)
+
+		# redirects to index
+		redirect_to '/creatures'    
+	end
+
+	# display a specific creature
+	def show
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :show
+	end
+
+	# shows a form to capture edit data
+	def edit
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
+	end
+
+	# collect updated info from edit form, then save, then redirect to index to show all
+	def update
+		id = params[:id]
+		creature = Creature.find(id)
+
+		# get updated attributes from form
+		updated_info = params.require(:creature).permit(:name, :description)
+		# save updated attributes
+		creature.update_attributes(updated_info)
+
+		redirect_to "/creatures/#{creature.id}"
+	end
+
+	# method to destroy current creature.
+	def destroy
+		id = params[:id]
+		creature = Creature.find(id)
+
+		creature.destroy
+
+		# redirects to index
+		redirect_to '/creatures'    
 	end
 
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,25 @@ CreatureApp::Application.routes.draw do
 
   root to: "creatures#index"
 
-  # just to be ReSTful
-  get '/creatures', to: "creatures#index"
+  # GET - list all creatures
+  get '/creatures', to: "creatures#list"
 
   # GET - requesting a page with a form, for new creature
   get '/creatures/new', to: "creatures#new"
 
+  # GET - a request to show a single/specific creature
+  get '/creatures/:id', to: "creatures#show"
+
+  # GET - a request to get edit form for a specific existing creature
+  get '/creatures/:id/edit', to: "creatures#edit"
+
   # POST - the form has submitted a post with new creature data
   post '/creatures', to: "creatures#create"
+
+  # POST - delete current creature
+  post '/creatures/:id/destroy', to: "creatures#destroy"
   
+  # PUT (pseudo) - update using the data from the edit form
+  put '/creatures/:id', to: "creatures#update"
+
 end
